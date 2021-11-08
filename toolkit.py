2968702e3f8e3e967983a32c0b6778b5492cb23f
basketcase03 <muskankhajuria03@gmail.com>
2021-11-08 19:29:55 +0530

    added funcs

--- toolkit.py
+++ toolkit.py
@@ -1,3 +1,31 @@
+import os
+import argparse
+
+def check_file(fname):
+    return os.path.isfile(fname) 
+
+def check_dir(dname):
+    return os.path.isdir(dname) 
+    
+
+def preprocess():
+    pass
+
+def stem():
+    pass
+
+def similar():
+    pass
+
+def txtsearch():
+    pass
+
+def visualisefile():
+    pass
+
+def visualisedir():
+    pass
+
 def main():
 
     parser = argparse.ArgumentParser(description = "Information Retrieval Toolkit")
@@ -6,7 +34,7 @@ def main():
                         metavar = "file_name", 
                         help = "Stores stemmed version of file in same dir.")
   
-    parser.add_argument("-c", "--similar", type = str, nargs = 2,
+    parser.add_argument("-s", "--similar", type = str, nargs = 2,
                        metavar = ('sim_type','doc_or_file'),
                        help = "Shows similarity score btw files.\
                            c: cosine \
@@ -20,25 +48,22 @@ def main():
                         metavar = ('txt','file1'), 
                         help = "Returns if text present in given document.")
     
-    parser.add_argument("-v", "--visualisefile", type = str, nargs = 1,
+    parser.add_argument("-f", "--visualisefile", type = str, nargs = 1,
                         metavar = 'file_name',
                         help = "Saves visualisations of n-grams.")
 
-    parser.add_argument("-v", "--visualisedir", type = str, nargs = 1,
+    parser.add_argument("-d", "--visualisedir", type = str, nargs = 1,
                         metavar = 'dir_name',
                         help = "Saves visualisations of similarity scores.")
   
     args = parser.parse_args()
       
-    # calling functions depending on type of argument
     if args.stem != None:
         stem(args)
     if args.similar != None:
         similar(args)
     if args.txtsearch !=None:
         txtsearch(args)
-    if args.alldir != None:
-        alldir(args)
     if args.visualisefile != None:
         visualisefile(args)
     if args.visualisedir != None:
