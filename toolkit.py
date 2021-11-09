a602d94735a3e6e736283242cdb7a5f193cbd874
basketcase03 <muskankhajuria03@gmail.com>
2021-11-09 21:39:39 +0530

    final

--- toolkit.py
+++ toolkit.py
@@ -14,8 +14,8 @@ if __name__ == "__main__":
     parser.add_argument("-s", "--similar", type = str, nargs = 2,
                        metavar = ('sim_type','doc_or_file'),
                        help = "Shows similarity score btw files.\
-                           c: cosine, types = B,T\
-                           e: euclidean, types = B,T \
+                           c: cosine, (Furthermore types = B,T)\
+                           e: euclidean, (Furthermore types = B,T) \
                            j: jaccard \
                             \
                            0: all in dir \
@@ -25,14 +25,19 @@ if __name__ == "__main__":
     parser.add_argument("-t", "--txtsearch", type = str, nargs = 2,
                         metavar = ('txt','file1'), 
                         help = "Returns if text present in given document. \
-                            b:brute force\
+                            Furthermore\
+                           b:brute force\
                            k: KMP \
                            r: Rabin Karp ")
 
     
     parser.add_argument("-f", "--visualisefile", type = str, nargs = 2,
                         metavar = ('file1','file2'), 
-                        help = "Visualisation of similarity scores")
+                        help = "Visualisation of similarity scores\
+                            Furthermore: \
+                           c: cosine, (Furthermore types = B,T)\
+                           e: euclidean, (Furthermore types = B,T) \
+                           j: jaccard ")
   
     args = parser.parse_args()
       
