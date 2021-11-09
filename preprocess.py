76469e733f0c928ecc53b89c89b1b4ca49b83e93
basketcase03 <muskankhajuria03@gmail.com>
2021-11-09 10:48:26 +0530

    sim and preprocess

---
+++ preprocess.py
@@ -0,0 +1,19 @@
+import nltk
+import string
+from nltk.corpus import stopwords
+
+def preprocess(text):
+    from nltk.tokenize import word_tokenize
+    tokens = word_tokenize(text)
+    # convert to lower case
+    tokens = [w.lower() for w in tokens]
+    # remove punctuation from each word
+    table = str.maketrans('', '', string.punctuation)
+    stripped = [w.translate(table) for w in tokens]
+    # remove remaining tokens that are not alphabetic
+    words = [word for word in stripped if word.isalpha()]
+    # filter out stop words
+    stop_words = set(stopwords.words('english'))
+    words = [w for w in words if not w in stop_words]
+    txt = ' '.join(words)
+    return txt
--- toolkit.py
+++ toolkit.py
@@ -1,21 +1,77 @@
 import os
 import argparse
+from numpy.core.einsumfunc import einsum
+from similarities import cosine_sim, euclidean_sim, jaccard_sim
+from preprocess import preprocess
 
 def check_file(fname):
     return os.path.isfile(fname) 
 
 def check_dir(dname):
     return os.path.isdir(dname) 
-    
 
-def preprocess():
-    pass
+def get_files(dir_name):
+    file_list = []
+    for myfile in os.listdir(dir_name):
+        if file.endswith(".txt"):
+            file_list.append(os.path.join(dir_name, myfile))
+    return file_list
+    
 
 def stem():
     pass
 
-def similar():
-    pass
+def print_sim(sim_type,file1,file2):
+    print("File 1: ".file1)
+    print("File 2: "file2,)
+    sim = ""
+    score = 0
+
+    txt1 = file1.read()
+    txt2 = file2.read()
+
+    txt1 = preprocess(txt1)
+    txt2 = preprocess(txt2)
+
+    if sim_type == 'c':
+        sim = "cosine"
+        score = cosine_sim(txt1,txt2)
+    elif sim_type == 'e':
+        sim = "euclidean"
+        score = euclidean_sim(txt1,txt2)
+    else:
+        sim = "jaccard"
+        score = jaccard_sim(txt1,txt2)
+    print("Similarity score (sim_} is : {score_}".format(sim_=sim,score_=score))
+
+def similar(args):
+     c: cosine \
+                           e: euclidean \
+                           j: jaccard \
+                            \
+                           0: dir \
+                           1: files")
+
+
+    sim_type = args.similar[0]
+    dir_or_file = args.similar[1]
+
+    if dir_or_file == 0:  #directory
+        dir_name = input("Enter dir name: ")
+        if not check_dir(dir_name):
+            print("Enter valid dir name")
+            return
+        file_list = get_files(dir_name)
+        for file1 in file_list:
+            for file2 in file_list:
+                print_sim(sim_type,file1,file2)
+    else:
+        file1 = input("Enter file1: ")
+        file2 = input("Enter file2: ")
+        if (not check_file(file1)) or (not check_file(file2)):
+            print("Enter valid file name")
+            return 
+        print_sim(sim_type,file1,file2)
 
 def txtsearch():
     pass
